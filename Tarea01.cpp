f814ff94f011dd6d756dec58b21353aa826cf06f
neperezl <101434538+neperezl@users.noreply.github.com>
2022-03-22 21:32:23 -0500

    Update Tarea01.cpp

--- Tarea01.cpp
+++ Tarea01.cpp
@@ -5,11 +5,12 @@ using namespace std;
 
 int main(){
     int n = 0;
+    int min = 10;
+    int max = 100;
     double result = 0;
-    for (n = 10; n <=100; n = n+1){
+    for (n = min; n <=max; n = n+1){
         if (n%2 ==1) {
             result = result + 1.0/n;
-            //std::cout << n << "  " result <<"\n" ;
         }
     }
 std::cout << "  "<< result <<"\n";
